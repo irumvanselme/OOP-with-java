6dd7296686766034633c1db80b64306ea9f48b46
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-11 09:56:20 +0200

    Make the StudentController very fine

--- JavaOnWeb/src/main/java/edu/school/JavaOnWeb/controllers/StudentController.java
+++ JavaOnWeb/src/main/java/edu/school/JavaOnWeb/controllers/StudentController.java
@@ -1,13 +1,17 @@
-package com.example.JavaOnWeb.controllers;
+package edu.school.JavaOnWeb.controllers;
 
 import java.sql.SQLException;
 import java.util.List;
 
-import com.example.JavaOnWeb.dao.StudentDAO;
-import com.example.JavaOnWeb.models.Student;
+import edu.school.JavaOnWeb.dao.StudentDAO;
+import edu.school.JavaOnWeb.models.Student;
 
 public class StudentController {
-    private final StudentDAO dao = new StudentDAO();
+    private final StudentDAO dao;
+
+    public StudentController() throws SQLException {
+        dao = new StudentDAO();
+    }
 
     public List<Student> getStudents() throws SQLException {
         return dao.getAll();
@@ -16,4 +20,6 @@ public class StudentController {
     public int create(String firstName, String lastName, String gender, String email, String year, String className) throws SQLException {
         return dao.create(new Student(firstName, lastName, gender, email , year, className));
     }
+
+
 }
